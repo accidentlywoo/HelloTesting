bc3610fc2661f3366909e383e6ea4fb897157fdb
accidentlywoo <accidentlywoo@gmail.com>
2020-10-18 17:01:57 +0900

    @DisplatName을 사용하지 않고 메소드명으로 표현할때, 가독성으 높이는 설정 @DisplayNameGeneration

--- src/test/java/woo/accidentlywoo/junittest/StudyTest.java
+++ src/test/java/woo/accidentlywoo/junittest/StudyTest.java
@@ -1,15 +1,18 @@
 package woo.accidentlywoo.junittest;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 만들기 ╯°□°）╯")
+//    @DisplayName("스터디 만들기 ╯°□°）╯")
     public void create_new_study() throws Exception{
         //given
         Study study = new Study(10);
