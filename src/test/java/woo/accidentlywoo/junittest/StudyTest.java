6c90a8ddd82759c4745dd539aae5b93de000b727
accidentlywoo <accidentlywoo@gmail.com>
2020-10-18 16:59:07 +0900

    Jupiter 이용한 간단한 Test코드와 테스트 @DisplayName feat. 꿀귀 표현

--- src/test/java/woo/accidentlywoo/junittest/StudyTest.java
+++ src/test/java/woo/accidentlywoo/junittest/StudyTest.java
@@ -1,16 +1,21 @@
 package woo.accidentlywoo.junittest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyTest {
+
     @Test
+    @DisplayName("스터디 만들기 ╯°□°）╯")
     public void create_new_study() throws Exception{
         //given
-
+        Study study = new Study(10);
         //when
-
+        assertNotNull(study);
         //then
+        System.out.println("create");
     }
 }
